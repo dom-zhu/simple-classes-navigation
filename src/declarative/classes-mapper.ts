4e1da14f85ad7a48e6ef4f730d2ffcc995306787
Dom Zhu <dominic.zhu@patientzero.com.au>
2022-06-11 20:53:34 +1000

    done

--- src/declarative/classes-mapper.ts
+++ src/declarative/classes-mapper.ts
@@ -1,6 +1,67 @@
-import { ISchoolWithClasses, INavLinkGroup } from "../types";
+import { groupBy } from "../helpers/classes.helper";
+import { ISchoolWithClasses, INavLinkGroup, IClass, INavLink } from "../types";
+
+const yearLevelGroupingThreshold = 6;
 
 export const createClassesNavigation = (schools: ISchoolWithClasses[]): INavLinkGroup[] => {
-    // TODO: to be implemented
-    return [null];
+    if (schools.length === 1) return onlySchool(schools[0]);
+
+    return multipleSchools(schools);
+};
+
+const onlySchool = (school: ISchoolWithClasses): INavLinkGroup[] => {
+    if (school.classes.length === 0) return [];
+
+    return [
+        {
+            links: generateLinks(school.classes),
+        },
+    ];
+};
+
+const multipleSchools = (schools: ISchoolWithClasses[]): INavLinkGroup[] => {
+    const transformedSchools: INavLinkGroup[] = schools
+        .map(({ classes }, index) => {
+            // don't display school which has no classes
+            if (!classes || classes.length === 0) return;
+
+            return {
+                name: `School ${index + 1}`,
+                links: generateLinks(classes),
+            };
+        })
+        .filter(Boolean);
+
+    // don't show school name for only one school with classes
+    if (transformedSchools.length === 1) return singleSchoolWithClasses(transformedSchools);
+
+    return transformedSchools;
+};
+
+const singleSchoolWithClasses = (schools: INavLinkGroup[]) => {
+    const [school] = schools;
+    const { links } = school;
+    return [{ links }];
+};
+
+const generateLinks = (classes: IClass[]): INavLink[] => {
+    return classes.length > yearLevelGroupingThreshold ? yearLevelLinks(classes) : classLinks(classes);
+};
+
+const yearLevelLinks = (schoolClasses: IClass[]): INavLink[] => {
+    const classesGroupedByYearLevel = groupBy(schoolClasses, (c) => c.yearLevel);
+
+    return Object.entries(classesGroupedByYearLevel).map(([yearLevel, classes]) => ({
+        name: `Year ${yearLevel}`,
+        url: "",
+        links: classLinks(classes),
+    }));
+};
+
+const classLinks = (classes: IClass[]): INavLink[] => {
+    return classes.map((c, index) => ({
+        name: `Class ${index + 1}`,
+        url: "",
+        key: `Class ${index + 1}`,
+    }));
 };
--- src/imperative/ClassesMapper.ts
+++ src/imperative/ClassesMapper.ts
@@ -1,10 +1,90 @@
-import { ISchoolWithClasses, INavLinkGroup } from "../types";
+import { groupBy } from "../helpers/classes.helper";
+import { ISchoolWithClasses, INavLinkGroup, IClass, INavLink } from "../types";
 
 export class ClassesMapper {
-    constructor(schoolsWithClasses: ISchoolWithClasses[]) {}
+    private schoolsWithClasses: ISchoolWithClasses[];
+
+    private readonly yearLevelGroupingThreshold = 6;
+
+    constructor(schoolsWithClasses: ISchoolWithClasses[]) {
+        this.schoolsWithClasses = schoolsWithClasses;
+    }
 
     public createClassesNavigation(): INavLinkGroup[] {
-        // TODO: to be implemented
-        return [null];
+        if (this.schoolsWithClasses.length === 1) return this.onlySchool();
+
+        return this.multipleSchools();
     }
+
+    private onlySchool = (): INavLinkGroup[] => {
+        const [school] = this.schoolsWithClasses;
+        if (school.classes.length === 0) return [];
+
+        return [
+            {
+                links: this.generateLinks(school.classes),
+            },
+        ];
+    };
+
+    private multipleSchools = () => {
+        let schools: INavLinkGroup[] = [];
+
+        for (const [index, { classes }] of this.schoolsWithClasses.entries()) {
+            // don't display school which has no classes
+            if (!classes || classes.length === 0) continue;
+
+            schools = schools.concat({
+                name: `School ${index + 1}`,
+                links: this.generateLinks(classes),
+            });
+        }
+
+        // don't show school name for only one school with classes
+        if (schools.length === 1) return this.singleSchoolWithClasses(schools);
+
+        return schools;
+    };
+
+    private singleSchoolWithClasses = (schools: INavLinkGroup[]) => {
+        const [school] = schools;
+        const { links } = school;
+        return [{ links }];
+    };
+
+    private generateLinks = (classes: IClass[]): INavLink[] => {
+        return classes.length > this.yearLevelGroupingThreshold
+            ? this.yearLevelLinks(classes)
+            : this.classLinks(classes);
+    };
+
+    private yearLevelLinks = (schoolClasses: IClass[]): INavLink[] => {
+        const classesGroupedByYearLevel = groupBy(schoolClasses, (c) => c.yearLevel);
+
+        let yearLevelLinks: INavLink[] = [];
+
+        for (const [yearLevel, classes] of Object.entries(classesGroupedByYearLevel)) {
+            yearLevelLinks = yearLevelLinks.concat({
+                name: `Year ${yearLevel}`,
+                url: "",
+                links: this.classLinks(classes),
+            });
+        }
+
+        return yearLevelLinks;
+    };
+
+    private classLinks = (classes: IClass[]): INavLink[] => {
+        let classLinks: INavLink[] = [];
+
+        for (const [index] of classes.entries()) {
+            classLinks = classLinks.concat({
+                name: `Class ${index + 1}`,
+                url: "",
+                key: `Class ${index + 1}`,
+            });
+        }
+
+        return classLinks;
+    };
 }
